b01e92aac48ffcbded5c406f08dda76fe9f9b901
amananandrai <amananandrai@gmail.com>
2021-10-09 16:55:02 +0530

    Create amananand.cpp
    My first PR

---
+++ amananand.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"Hello Open Source Community"<<endl;
+  cout<<"My name is Aman Anand Rai"<<endl;
+	cout<<"This is my first PR!";
+	return 0;
+}
