a80f9b55773a4769642902e026c2af10b34bf4f3
Avinash Kumar <67888289+avi0901@users.noreply.github.com>
2021-10-16 20:21:41 +0530

    Create sex_disaggregated_data.py

---
+++ sex_disaggregated_data.py
@@ -0,0 +1,10 @@
+# Datasex contain data of sex_disaggregation due to COVID.
+
+print("Chutiya banaaya bada maja aaya")
+happy = False
+for i in range(2):
+  if not happy:
+    send mail @info@medicaps.ac.in
+    text = "Madarchod college taking offline exam"
+  else:
+    happy = !happy
