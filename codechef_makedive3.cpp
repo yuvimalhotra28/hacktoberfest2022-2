191446951df2ec0266db1230c90683bdc811c2a0
i-ankur-01 <87879435+i-ankur-01@users.noreply.github.com>
2021-10-18 21:54:20 +0530

    Create codechef_makedive3.cpp
    this is a code for the problem from codechef starters 12

---
+++ codechef_makedive3.cpp
@@ -0,0 +1,28 @@
+//this code is the solution for the codechef problem - https://www.codechef.com/START12C/problems/MAKEDIV3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n==1){
+            cout<<"3"<<endl;
+        }
+        else if(n==2){
+            cout<<"15"<<endl;
+        }
+        else{
+            cout<<"1";
+            for(int i =1 ; i<=n-2 ;i++){
+                cout<<"0";
+            }
+            cout<<"5"<<endl;
+        }
+    }
+    return 0;
+}
