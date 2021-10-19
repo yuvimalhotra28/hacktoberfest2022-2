12cb4fb8a95d30db058b4826ed10b11d0eabde9f
Sanchit <sanchit02@outlook.com>
2021-10-19 18:49:13 +0530

    Added many changes into the file: 1. Add the optimal folder structure. 2. Added SubListSearch.js file. 3. Fixed codes that were broken.

--- SortingAlogorithm/SelectionSort.java
+++ SortingAlogorithm/SelectionSort.java
@@ -1,21 +1,23 @@
-class SelectionSort{
-	public static void main(String[]args){
-		int[] a = {6,4,3,8,7,9,2,24,89,43,1,80,54};
+package SortingAlogorithm;
 
-		int min=0;
-		int temp=0;
-		for(int i=0; i<a.length-1; i++){
+class SelectionSort {
+	public static void main(String[] args) {
+		int[] a = { 6, 4, 3, 8, 7, 9, 2, 24, 89, 43, 1, 80, 54 };
+
+		int min = 0;
+		int temp = 0;
+		for (int i = 0; i < a.length - 1; i++) {
 			min = a[i];
-			for(int j=i; j<a.length; j++){
-				if(min>a[j]){
+			for (int j = i; j < a.length; j++) {
+				if (min > a[j]) {
 					temp = a[i];
 					a[i] = a[j];
-					a[j] = temp; 
+					a[j] = temp;
 				}
 			}
 		}
 
-		for(int i=0; i<a.length; i++){
+		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
 	}
