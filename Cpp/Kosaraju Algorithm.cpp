8f5cf8fad69723f6445bfcd6ddce85b2fdc84408
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-10-02 11:22:49 +0530

    Create Kosaraju Algorithm.cpp

---
+++ Cpp/Kosaraju Algorithm.cpp
@@ -0,0 +1,87 @@
+// Kosaraju Algorithm - By Sameer Aggrawal
+
+// This is logic of Kosaraju Algorithm, Call the Kosaraju function with parameters - (total vertices, adjacency list).
+// It returns the total number of strongly connected components.
+
+// Similar to topological sort function (dfs)
+void dfs(int node,vector<int> adj[],vector<bool> &visited,stack<int> &stk)
+{
+    visited[node] = true;
+    
+    for(auto child: adj[node])
+    {
+        if(!visited[child])
+        {
+            dfs(child,adj,visited,stk);
+        }
+    }
+    
+    stk.push(node);
+}
+	
+// Normal DFS function
+void dfsRec(int node,vector<int> g[],vector<bool> &visited)
+{
+    visited[node]=true;
+    
+    for(auto child: g[node])
+    {
+        if(!visited[child])
+        {
+            dfsRec(child,g,visited);
+        }
+    }
+    
+    return;
+}
+	
+//Function to find number of strongly connected components in the graph.
+int kosaraju(int V, vector<int> adj[])
+{
+    int i,j;
+    
+    stack<int> stk;
+    
+    vector<int> result;
+    vector<bool> visited(V,false);
+    
+    for(i=0;i<V;i++)
+    {
+        if(!visited[i])
+        {
+            dfs(i,adj,visited,stk);
+        }
+    }
+    
+    while(!stk.empty())
+    {
+        result.push_back(stk.top());
+        stk.pop();
+    }
+    
+    vector<int> g[V];
+    
+    for(i=0;i<V;i++)
+    {
+        for(auto x: adj[i])
+        {
+            g[x].push_back(i);
+        }
+    }
+    
+    for(i=0;i<V;i++) visited[i]=false;
+    
+    int cnt=0;
+    
+    for(i=0;i<V;i++)
+    {
+        int node=result[i];
+        if(!visited[node])
+        {
+            cnt++;
+            dfsRec(node,g,visited);
+        }
+    }
+    
+    return cnt;
+}
