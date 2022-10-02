01122c6049d6b2633912a7d7c44ee9b255d90177
Shree <shreekumarf@gmail.com>
2022-10-02 09:48:53 +0530

    Spiral Order Matrix

---
+++ Cpp/spiralOrderMatrix.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "Enter size of matrix:\n";
+    cin >> n >> m;
+    int arr[n][m];
+    cout << "Enter elements in the matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout<< "spirally traversing the matrix: "<<endl;
+    //Spiral Order of matrix logic:
+    int row_start = 0, row_end = n - 1, column_start = 0, column_end = m - 1;
+    while (row_start <= row_end && column_start <= column_end)
+    {
+        // for row start..
+        for (int col = column_start; col <= column_end; col++)
+        {
+            cout << arr[row_start][col] << " ";
+        }
+        row_start++;
+        //for column end..
+        for (int row = row_start; row <= row_end; row++)
+        {
+            cout << arr[row][column_end] << " ";
+        }
+        column_end--;
+        // for row_end...
+        for (int col = column_end; col >= column_start; col--)
+        {
+            cout << arr[row_end][col] << " ";
+        }
+        row_end--;
+        //for column_start..
+        for (int row = row_end; row >= row_start; row--)
+        {
+            cout << arr[row][column_start] << " ";
+        }
+        column_start++;
+    }
+    return 0;
+}
